f2a7604ffacea96e60315ca82837d845c1c320db
Wilian Liz <wliz2726@gmail.com>
2020-06-13 12:25:29 -0400

    Cambios en Footer

--- catalog/language/es-ES/common/footer.php
+++ catalog/language/es-ES/common/footer.php
@@ -1,7 +1,7 @@
 <?php
 $_['text_information'] 	=  'Información';
 $_['text_service'] 	=  'Asistencia al Cliente';
-$_['text_extra'] 	=  'Extras';
+$_['text_extra'] 	=  'Conéctate';
 $_['text_contact'] 	=  'Contáctenos';
 $_['text_return'] 	=  'Devoluciones';
 $_['text_sitemap'] 	=  'Mapa del sitio';
@@ -13,4 +13,4 @@ $_['text_account'] 	=  'Mi cuenta';
 $_['text_order'] 	=  'Historial de pedidos';
 $_['text_wishlist'] 	=  'Lista de deseos';
 $_['text_newsletter'] 	=  'Hoja informativa';
-$_['text_powered'] 	=  '%s &copy;%s Derechos reservados. Desarrollado por <a href="https://www.instagram.com/methedesigner/">me the designer</a>';
+$_['text_powered'] 	=  '%s &copy;%s Derechos reservados. Desarrollado por <a href="https://www.instagram.com/methedesigner/" target="_blank">me the designer</a>';
